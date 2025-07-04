46e6c29a1847a0b87488a0f6e2778e172ce5c0be
teto249 <118367287+teto249@users.noreply.github.com>
2025-07-05 05:33:21 +0800

    front

--- Frontend/next.config.mjs
+++ Frontend/next.config.mjs
@@ -4,10 +4,11 @@ const nextConfig = {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
   async rewrites() {
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
     return [
       {
         source: '/api/:path*',
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
+        destination: `${apiUrl}/api/:path*`,
       },
     ];
   },
